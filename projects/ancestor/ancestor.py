b2f38a07e22437445b159a4520a2d0d8f4034583
mmastin <matthew.mastin@gmail.com>
2020-02-21 15:43:00 -0500

    day 3/4 work

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,84 @@
+from util import Stack, Queue
+
+class GraphNode:
+    def __init__(self, value):
+        self.children = set()
+        self.parents = set()
+        self.value = value
+
+    def add_child(self, node):
+        self.children.add(node)
+        node.parents.add(self)
+
+    def __repr__(self):
+        children = [x.value for x in self.children]
+        parents = [x.value for x in self.parents]
+        return f'Node: {self.value} children {children} parents {parents}'
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def add_node(self, value):
+        if self.vertices.get(value, None) is None:
+            self.vertices[value] = GraphNode(value)
+        else:
+            return False
+
+    def add_edge(self, value1, value2):
+        if value1 not in self.vertices:
+            self.add_node(value1)
+        if value2 not in self.vertices:
+            self.add_node(value2)
+        node1 = self.vertices[value1]
+        node2 = self.vertices[value2]
+        node1.add_child(node2)
+
+    def get_node(self, value):
+        return self.vertices.get(value, None)
+
+    def ancestor_paths(self, value):
+        node = self.get_node(value)
+        if node is None:
+            return None
+        s = Stack()
+        s.push([value])
+
+        ancestor_paths = []
+
+        while s.size() > 0:
+            path = s.pop()
+            node = self.get_node(path[-1])
+            for parent_node in node.parents:
+                new_path = path.copy()
+                new_path.append(parent_node.value)
+                s.push(new_path)
+                ancestor_paths.append(new_path)
+
+        return ancestor_paths
+
+        def __repr__(self):
+            for value in self.vertices:
+                print(self.vertices[value])
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    g = Graph()
+    for pair in ancestors:
+        parent_value = pair[0]
+        child_value = pair[1]
+        g.add_edge(parent_value, child_value)
+
+    ancestor_paths = g.ancestor_paths(starting_node)
+
+    longest_path = []
+    for path in ancestor_paths:
+        if len(path) > len(longest_path):
+            longest_path = path
+        elif len(path) == len(longest_path)
+        if path[-1] < longest_path[-1]:
+            longest_path = path
+
+    if len(longest_path) == 0:
+        return -1
+    else:
+        return longest_path
--- projects/social/social.py
+++ projects/social/social.py
@@ -1,5 +1,18 @@
 import random
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -17,12 +30,15 @@ class SocialGraph:
         Creates a bi-directional friendship
         """
         if user_id == friend_id:
-            print("WARNING: You cannot be friends with yourself")
+            # print("WARNING: You cannot be friends with yourself")
+            return False
         elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
-            print("WARNING: Friendship already exists")
+            # print("WARNING: Friendship already exists")
+            return False
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
+            return True
 
     def add_user(self, name):
         """
@@ -75,6 +91,26 @@ class SocialGraph:
         # total friendships = avg_friendships * num_users
         # N = avg_friendships * num_users // 2
 
+    def populate_graph_linear(self, num_users, avg_friendships):
+        self.last_id = 0
+        self.users = {}
+        self.friendships = {}
+
+        for i in range(num_users):
+            self.add_user(f'User {i + 1}')
+
+        friendships_to_create = avg_friendships * num_users
+        friendships = 0
+        collisions = 0
+        while friendships < friendships_to_create:
+            user_id = random.randint(1, self.last_id)
+            friend_id = random.randint(1, self.last_id)
+            if self.add_friendship(user_id, friend_id):
+                friendships += 2
+            else:
+                collisions += 1
+        print(f'COLLISIONS: {collisions}')
+
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
@@ -84,14 +120,50 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
-        visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
+        # create an empty queue
+        q = Queue()
+        # add a path to queue
+        q.enqueue( [user_id] )
+        # create an empty set to store visited nodes
+        visited = {}
+        # while queu is not empty
+        while q.size() > 0:
+            # dequeue the first vertex
+            path = q.dequeue()
+            # GRAB THE LAST VERTEX FROM THE PATH
+            v = path[-1]
+            # check if it's been visited
+            # if has not been visited
+            if v not in visited:
+                # mark it as visited
+                # add it to the visited dictionary, with the path as the key
+                visited[v] = path
+                # then add all FRIENDS to back of queue
+                # for friend_id in self.get_friends(v):
+                for friend in self.friendships[v]:
+                    # COPY THE PATH
+                    path_copy = path.copy()
+                    # ADD FRIEND TO THE PATH
+                    path_copy.append(friend)
+                    q.enqueue(path_copy)
+
         return visited
 
+import time
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
-    print(sg.friendships)
-    connections = sg.get_all_social_paths(1)
-    print(connections)
+
+    start_time = time.time()
+    sg.populate_graph(100, 90)
+    end_time = time.time()
+    print(f'quadratic runtime: {end_time - start_time} seconds')
+
+    start_time = time.time()
+    sg.populate_graph_linear(100, 90)
+    end_time = time.time()
+    print(f'linear runtime: {end_time - start_time} seconds')
+
+    # print(sg.friendships)
+    # connections = sg.get_all_social_paths(1)
+    # print(connections)
